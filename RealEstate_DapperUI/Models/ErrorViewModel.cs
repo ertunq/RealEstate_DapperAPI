c0d2b99dc371352e137a839b7d08bfc3637c8560
Ertugrul <ertunq@gmail.com>
2023-08-15 11:08:53 +0300

    Add project files.

---
+++ RealEstate_DapperUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RealEstate_DapperUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
